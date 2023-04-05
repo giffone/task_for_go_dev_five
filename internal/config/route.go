0ccd84c7a33068e1b3b2c710f863b862e18acd4a
giffone <galimzhanov.faizulla@gmail.com>
2023-04-05 22:54:29 +0600

    add: parse config

--- internal/config/route.go
+++ internal/config/route.go
@@ -1,6 +1,15 @@
 package config
 
+import "errors"
+
 type Route struct {
 	Host string `json:"host"`
 	Port string `json:"port"`
+}
+
+func (r Route) validate() error {
+	if r.Host == "" || r.Port == "" {
+		return errors.New("empty route config")
+	}
+	return nil
 }
